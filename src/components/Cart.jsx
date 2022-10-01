fb40a287e8930f1dedc42f173a7d95e4a3f3d48e
Carolina Lezcano <lezcanocarolina99@gmail.com>
2022-10-01 19:35:38 -0300

    item collection II

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,45 @@
+import { increment, serverTimestamp } from "firebase/firestore";
 import { useContext } from "react";
 import { CartContext } from "./CartContext";
+import { serverTimestamp, doc, setDoc, collection, increment, updateDoc } from "firebase/firestore";
+import db from "../utils/firebaseConfig";
 
 const Cart = () => {
     const myContext = useContext(CartContext);
     console.log(myContext.cartList)
+
+    const createOrder = async () => {
+        const itemsForDB = test.cartList.map(item => ({
+            id: item.id,
+            title: item.title,
+            price: item.price,
+            quantity: item.quantity
+        }))
+        let order = {
+            buyer: {
+                nombre: "Amira Nahir",
+                email: "amiranahir@gmail.com",
+                phone: "1165489623"
+            },
+            items: itemsForDB,
+            date: serverTimestamp(),
+            total: test.totalCantidad()
+
+        }
+        const newOrderRef = doc(collection(db, item.id))
+        await setDoc(newOrderRef, order)
+
+        test.cartList.forEach( async (item) => {
+            const itemRef = doc(db, "productos", "id");
+            await updateDoc(itemRef, {
+                stock: increment(-item.quantity)
+            });
+        })
+        test.removeItem()
+
+        alert("Orden creada: " + newOrderRef.id)
+    }
+
     return (
         <>
             <h1>I'm Cart</h1>
@@ -17,6 +53,7 @@ const Cart = () => {
                             <img src={item.img}></img>
                             <button className="btn" onClick={() => { myContext.removeItem(item.id) }}>Borrar este producto</button></li>)
                     }
+                    <button onClick={createOrder}>Crear orden</button>
                 </>
                 : <h2>No hay productos</h2>}
         </>
